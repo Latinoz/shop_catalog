f6ef45b4593f62394d31b570ddd200b9df0fa251
Latinoz <lcsm@rambler.ru>
2015-12-22 22:36:19 +0300

    Create add_product.php

---
+++ add_product.php
@@ -0,0 +1,43 @@
+<?php
+
+include "lib.inc.php";
+include "config.inc.php";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Форма добавления товара в каталог</title>
+</head>
+<body>
+	<form action="save_product.php" method="post">
+		<p>Наименование товара: <input type="text" name="title" size="40">
+		
+		<p>Категория товара: <select name="catselect">
+        <option value="0">Выберите категорию</option>
+        <?php
+	    $sql = 'SELECT * FROM category';
+	
+	    $res = mysqli_query($link,$sql);
+        while($row = mysqli_fetch_assoc($res)){
+	    
+        
+	 echo "<option value='".$row['ID']."'>".$row['name']."</option>";
+    
+     }
+	?>
+     </select>
+		
+		<p>Вес товара, кг: <input type="text" name="netto" size="14">
+		<p><input type="submit" value="Добавить">
+	</form>
+	
+	<br>
+	</br>
+	
+	<a href="catalog_prod.php">Вернуться в каталог</a>
+</body>
+</html>
+
